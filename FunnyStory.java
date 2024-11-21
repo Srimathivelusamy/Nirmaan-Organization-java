b4e330917d0cd867bccd341b34d95422d26b8dad
Srimathivelusamy <srimathivelusamy13@gmail.com>
2024-11-21 12:46:13 +0530

    Day-2

---
+++ FunnyStory.java
@@ -0,0 +1,24 @@
+public class FunnyStory
+{
+public static void main(String args[])
+{
+String name1 = "Srimathi";
+String adjective1 = "Happy";
+String adjective2 = "wonderful";
+String noun1 = "birds";
+String noun2 = "Chocolate";
+String verb1 = "Jump";
+String noun3 = "piano";
+String noun4 = "Children";
+String adjective3 = "sad";
+String name2 = "Lingeshwari";
+String place1 = "London";
+String noun5 = "ice cream";
+int number = 2024;
+String noun6 = "Toys";
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2+ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to" + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ".Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a worldwhere " + noun6 + "s ruled the world.";
+
+System.out.println(story);
+}
+}
